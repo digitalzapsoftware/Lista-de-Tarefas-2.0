e59b102ac719c9f1533945cc6b799ce4992c1b06
Nilson <99387388+Evanilsonpg@users.noreply.github.com>
2022-08-27 21:55:29 -0300

    instalando dependencias do react

---
+++ todolist/src/themes/theme.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+
+}
+
+export const Theme: React.FC = ({ children }:any) => {
+    return (
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    )
+}
